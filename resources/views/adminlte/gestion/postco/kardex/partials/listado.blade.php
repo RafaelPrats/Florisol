6d1d8e403cdcea0bfb2d81bc0e17e9811e0b4f84
Rafael Prats <rafael.pratsrecasen@gmail.com>
2026-09-18 14:21:26 -0500

    rafa

--- resources/views/adminlte/gestion/postco/kardex/partials/listado.blade.php
+++ resources/views/adminlte/gestion/postco/kardex/partials/listado.blade.php
@@ -3,79 +3,96 @@
     desde <b>{{ explode(' del ', convertDateToText($desde))[0] }}</b>
     al <b>{{ explode(' del ', convertDateToText($hasta))[0] }}</b> en <b>{{ $bodega }}</b>
 </legend>
-<div style="overflow-y: scroll; overflow-x: scroll; max-height: 700px">
-    <table class="table-bordered" style="width: 100%; border: 1px solid #9d9d9d" id="table_listado">
-        <thead>
-            <tr class="tr_fija_top_0">
-                <th class="padding_lateral_5 bg-yura_dark" style="width: 150px">
-                    Fecha
-                </th>
-                <th class="padding_lateral_5 bg-yura_dark" style="width: 100px">
-                    Tipo
-                </th>
-                <th class="padding_lateral_5 bg-yura_dark">
-                    Documento
-                </th>
-                <th class="padding_lateral_5 bg-yura_dark">
-                    Detalle
-                </th>
-                <th class="padding_lateral_5 th_yura_green" style="width: 90px">
-                    Entrada
-                </th>
-                <th class="padding_lateral_5 bg-yura_warning" style="width: 90px">
-                    Salida
-                </th>
-                <th class="padding_lateral_5 bg-yura_dark" style="width: 90px">
-                    Saldo <i class="fa fa-fw fa-caret-right"></i>{{ $saldo }}
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($listado as $pos => $item)
-                <tr onmouseover="$(this).css('background-color', 'cyan')"
-                    onmouseleave="$(this).css('background-color', '')">
-                    <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                        {{ explode(' del ', convertDateToText($item->fecha))[0] }}
+@if (count($listado) > 0)
+    <div style="overflow-y: scroll; overflow-x: scroll; max-height: 700px">
+        <table class="table-bordered" style="width: 100%; border: 1px solid #9d9d9d" id="table_listado">
+            <thead>
+                <tr class="tr_fija_top_0">
+                    <th class="padding_lateral_5 bg-yura_dark" style="width: 150px">
+                        Fecha
                     </th>
-                    <th class="text-center text-sm" style="border-color: #9d9d9d">
-                        @if ($item->tipo == 'INGRESO')
-                            <span class="badge bg-yura_primary">
-                                {{ $item->tipo }}
-                            </span>
-                        @else
-                            <span class="badge bg-yura_warning">
-                                {{ $item->tipo }}
-                            </span>
-                        @endif
+                    <th class="padding_lateral_5 bg-yura_dark" style="width: 100px">
+                        Tipo
                     </th>
-                    <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                        {{ $item->concepto }}
-                        - {{ $item->documento }}
+                    <th class="padding_lateral_5 bg-yura_dark">
+                        Documento
                     </th>
-                    <th class="padding_lateral_5 text-sm" style="border-color: #9d9d9d">
-                        {{ $item->detalle }}
+                    <th class="padding_lateral_5 bg-yura_dark">
+                        Detalle
                     </th>
-                    <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                        @if ($item->tipo == 'INGRESO')
-                            {{ number_format($item->cantidad) }}
-                            @php
-                                $saldo += $item->cantidad;
-                            @endphp
-                        @endif
+                    <th class="padding_lateral_5 th_yura_green" style="width: 90px">
+                        Entrada
                     </th>
-                    <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                        @if ($item->tipo == 'SALIDA')
-                            {{ number_format($item->cantidad) }}
-                            @php
-                                $saldo -= $item->cantidad;
-                            @endphp
-                        @endif
+                    <th class="padding_lateral_5 bg-yura_danger" style="width: 90px">
+                        Salida
                     </th>
-                    <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                        {{ number_format($saldo) }}
+                    <th class="padding_lateral_5 bg-yura_dark" style="width: 90px">
+                        Saldo <i class="fa fa-fw fa-caret-right"></i>{{ $saldo }}
                     </th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @php
+                    $list_bg = [
+                        0 => '#ffffff',
+                        1 => '#dddddd',
+                    ];
+                    $bg_selected = 0;
+                    $fecha_anterior = $listado[0]->fecha;
+                @endphp
+                @foreach ($listado as $pos => $item)
+                    @php
+                        if ($fecha_anterior != $item->fecha) {
+                            $fecha_anterior = $item->fecha;
+                            $bg_selected = !$bg_selected;
+                        }
+                    @endphp
+                    <tr onmouseover="$(this).css('background-color', '#fff8d7')"
+                        onmouseleave="$(this).css('background-color', '{{ $list_bg[$bg_selected] }}')"
+                        style="background-color: {{ $list_bg[$bg_selected] }}">
+                        <th class="padding_lateral_5" style="border-color: #9d9d9d">
+                            {{ explode(' del ', convertDateToText($item->fecha))[0] }}
+                        </th>
+                        <th class="text-center text-sm" style="border-color: #9d9d9d">
+                            @if ($item->tipo == 'INGRESO')
+                                <span class="badge bg-yura_primary">
+                                    {{ $item->tipo }}
+                                </span>
+                            @else
+                                <span class="badge bg-yura_danger">
+                                    {{ $item->tipo }}
+                                </span>
+                            @endif
+                        </th>
+                        <th class="padding_lateral_5" style="border-color: #9d9d9d">
+                            {{ $item->concepto }}
+                            - {{ $item->documento }}
+                        </th>
+                        <th class="padding_lateral_5 text-sm" style="border-color: #9d9d9d">
+                            {{ $item->detalle }}
+                        </th>
+                        <th class="padding_lateral_5 text-color_yura" style="border-color: #9d9d9d">
+                            @if ($item->tipo == 'INGRESO')
+                                {{ number_format($item->cantidad) }}
+                                @php
+                                    $saldo += $item->cantidad;
+                                @endphp
+                            @endif
+                        </th>
+                        <th class="padding_lateral_5 color_text-yura_danger" style="border-color: #9d9d9d">
+                            @if ($item->tipo == 'SALIDA')
+                                {{ number_format($item->cantidad) }}
+                                @php
+                                    $saldo -= $item->cantidad;
+                                @endphp
+                            @endif
+                        </th>
+                        <th class="padding_lateral_5" style="border-color: #9d9d9d">
+                            {{ number_format($saldo) }}
+                        </th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endif
